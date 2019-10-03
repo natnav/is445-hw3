46913341b907e4df12aee27dc40a473c9884481a
Nat <nat.jnava@gmail.com>
2019-10-02 21:15:50 -0700

    ok

--- hw3.js
+++ hw3.js
@@ -1,42 +1,52 @@
 
- const org1_depts = [
-    {
-      deptOne: 'accounting',
-      children: [
-        { name: 'accounting payable', children: [] },
-        { name: 'accounting receivable', children: [] },
-      ],
-    },
-    {
-      name: 'finance',
-      children: [],
-    },
-  ]
-  
-  const org2_depts = [
-    {
-      name: 'accounting',
-      children: [
-        { name: 'accounting payable', children: [] },
-        {
-          name: 'accounting receivable',
-          children: [{ name: 'cash', children: [] }, { name: 'check', children: [] }],
-        },
-      ],
-    },
-    {
-      name: 'finance',
-      children: [{ name: 'investment', children: [] }],
-    },
-  ]
+const org1_depts = [
+  {
+    name: 'accounting',
+    children: [
+      { name: 'accounting payable', children: [] },
+      { name: 'accounting receivable', children: [] },
+    ],
+  },
+  {
+    name: 'finance',
+    children: [],
+  },
+]
 
-  
+const org2_depts = [
+  {
+    name: 'accounting',
+    children: [
+      { name: 'accounting payable', children: [] },
+      {
+        name: 'accounting receivable',
+        children: [{ name: 'cash', children: [] }, { name: 'check', children: [] }],
+      },
+    ],
+  },
+  {
+    name: 'finance',
+    children: [{ name: 'investment', children: [] }],
+  },
+]
 
-
-  const message = prompt("What is the organization department?") 
 
 // alert('Your input is: ' + message)
-const org1_depts = document.getElementById("org1_depts").value;
-output.innerHTML = org1_depts;
+/*
+const org = document.getElementById('org').value
+org.innerHTML = 'Hello ' + message 
+*/
 
+const start_tag = '<ol>'
+const end_tag = '</ol>'
+const start_li = '<li>'
+const end_li = '</li>'
 
+var output = start_tag
+for (const user of org1_depts) {
+  output += start_li + user.name + end_li
+}
+output += end_tag
+
+const outputDiv = document.getElementById('output')
+outputDiv.innerHTML = output
