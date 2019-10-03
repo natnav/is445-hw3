71d76559f89cdf7e552cccddb2ed695de071e5ac
Nat <nat.jnava@gmail.com>
2019-10-02 21:25:10 -0700

    ok

--- hw3.js
+++ hw3.js
@@ -36,6 +36,9 @@ const org2_depts = [
 const org = document.getElementById('org').value
 org.innerHTML = 'Hello ' + message 
 */
+const message = prompt("What's your name?")
+
+// alert('Your input is: ' + message)
 
 const start_tag = '<ol>'
 const end_tag = '</ol>'
