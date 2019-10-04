b30e1a28d38935234409d99ee49ff9491f566d9e
Nat <nat.jnava@gmail.com>
2019-10-03 20:31:28 -0700

    fix

--- hw3.js
+++ hw3.js
@@ -1,14 +1,3 @@
-/*
-<ul>  
-  <li>1</li>
-  <li>2</li>
-  <li>xx</li>
-  <li>4</li>
-  <li>5</li>
-</ul>
-*/
-
-
 const depts = [
   {
     name: 'account',
@@ -16,38 +5,43 @@ const depts = [
   },
   {
     name: 'it',
-  
+
     children: [
-      (
-            name: 'crm',
-      ),
-      (     
-            name: 'ecommerce',
-      )
-    ]
+      {
+        name: 'crm',
+      },
+      {
+        name: 'ecommerce',
+      },
+    ],
+  },
+  {
+    name: 'finance',
+    children: [],
   },
 ]
-
 
 let result = '<ul>'
 
 for (const dept of depts) {
-  const deptStr = "<li>" + dept.name + "</li>"
+  //   const deptStr = `<li> ${dept.name} </li>`
+  const deptStr = '<li>' + dept.name + '</li>'
+  result += deptStr
 
   if (dept.children.length > 0) {
-      let childStr = "<ul>"
-      for (const child or dept.children) {
-        childStr += '<li>' + child.name + '</li>' /* $(dept.name) this is the same, but it is now a java code not string. it will run through ant variable*/
-      }
-      childStr += "<ul>"
-      result += childStr
-  )
-  result = result + deptStr 
-  /* can use += instead of = result + deptStr means the same thing */
+    let childStr = '<ul>'
+    for (const child of dept.children) {
+      childStr += '<li>' + child.name + '</li>' /* $(dept.name) this is the same, but it is now a java code not string. it will run through ant variable*/
+    }
+    childStr += '</ul>'
+    result += childStr /* can use += instead of = result + deptStr means the same thing */
+  }
 }
 
-result += "</ul>"
+result += '</ul>'
 
 //console.log(result)
+
 const divElement = document.getElementById('output')
 divElement.innerHTML = result
+
